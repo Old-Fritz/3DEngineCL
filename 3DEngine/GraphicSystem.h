d2eef3183a317f90a8cf86efdd412ad8119ab317
old-fritz <komarov.99@yandex.ru>
2019-01-12 01:23:34 +0300

    add interface for graphic system

---
+++ 3DEngine/GraphicSystem.h
@@ -0,0 +1,16 @@
+#ifndef GRAPHIC_SYSTEM_H
+#define GRAPHIC_SYSTEM_H
+#include "OpenCL.h"
+#include "Math3D.h"
+
+/// Control of graphic global settings
+
+int grInitGraphicSystem();
+
+m3dMatrix* grGetWorldMatrix(m3dMatrix* outMatrix);
+m3dMatrix* grGetProjectionMatrix(m3dMatrix* outmatrix);
+
+cl_mem grGetDepthBuffer();
+int grClearBackBuffer(m3dVector4 color);
+
+#endif
--- 3DEngine/Math3D.h
+++ 3DEngine/Math3D.h
@@ -1,5 +1,5 @@
-#ifndef 3DMATH_H
-#define 3DMATH_H
+#ifndef MATH3D_H
+#define MATH3D_H
 #include "vector2.h"
 #include "vector3.h"
 #include "vector4.h"
