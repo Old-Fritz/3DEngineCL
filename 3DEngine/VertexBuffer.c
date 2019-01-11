03cbbebb135964b86d9993a4c8b58a73d362e14a
old-fritz <komarov.99@yandex.ru>
2019-01-11 23:43:15 +0300

    add primitive topology in vertex buffer

--- 3DEngine/VertexBuffer.c
+++ 3DEngine/VertexBuffer.c
@@ -5,4 +5,5 @@ struct _grVertexBuffer
 	cl_mem clBuffer;
 	int vertexCount;
 	size_t vertexSize;
+	grPrimitiveTopology topology;
 };
--- 3DEngine/VertexBuffer.h
+++ 3DEngine/VertexBuffer.h
@@ -5,10 +5,16 @@
 
 /// Buffer in graphic devices that contains data about verticies
 
+typedef enum grPrimitiveTopology{
+	GR_PRIMITIVE_TOPOLOGY_POINT,
+	GR_PRIMITIVE_TOPOLOGY_LINE,
+	GR_PRIMITIVE_TOPOLOGY_TRIANGLE
+} grPrimitiveTopology;
+
 typedef struct _grVertexBuffer* grVertexBuffer;
 
 // Create buffer
-int grCreateVertexBuffer(grVertexBuffer* buffer, int vertexCount, size_t vertexSize);
+int grCreateVertexBuffer(grVertexBuffer* buffer, int vertexCount, size_t vertexSize, grPrimitiveTopology topology);
 // Release buffer
 int grShutdownVertexBuffer(grVertexBuffer buffer);
 // Write data to buffer from ptr
